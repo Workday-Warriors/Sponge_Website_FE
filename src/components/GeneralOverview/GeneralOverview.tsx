b1363ca76651fb425897ac7f354d31247a3f2949
Sherbek <mavlonovsherbek43@gmail.com>
2024-07-02 01:43:25 +0500

    fix: bugs

--- src/components/GeneralOverview/GeneralOverview.tsx
+++ src/components/GeneralOverview/GeneralOverview.tsx
@@ -12,10 +12,10 @@ export const GeneralOverview = () => {
   useEffect(() => {
     gsap.fromTo(
       person.current,
-      { x: -50 },
+      { x: -100 },
       {
         x: 0,
-        duration: 1.5,
+        duration: 2,
         scrollTrigger: {
           trigger: person.current,
         },
@@ -28,7 +28,7 @@ export const GeneralOverview = () => {
       {
         x: 0,
         opacity: 1,
-        duration: 1.5,
+        duration: 3,
         scrollTrigger: {
           trigger: card.current,
         },
@@ -46,42 +46,36 @@ export const GeneralOverview = () => {
       <div className='w-full flex flex-col xl:flex-row items-end h-full container px-5 xl:px-0 '>
         <div
           ref={person}
-          className=' w-full relative xl:w-1/2 h-full flex items-end pb-10'
+          className=' w-full  xl:w-1/2 h-full flex items-end pb-10'
         >
-          <img
-            src='./general.png'
-            alt=''
-            className=' w-[200px] xl:w-auto absolute bottom-[-400px] xl:static'
-          />
+          <img src='./general.png' alt='' />
         </div>
-        <div className='w-full relative h-full xl:w-1/2'>
+        <div
+          ref={card}
+          className=' w-full xl:w-1/2 h-full flex justify-center xl:justify-end items-end pb-20'
+        >
           <div
-            ref={card}
-            className=' h-full flex justify-center xl:justify-end items-end pb-0 xl:pb-20'
+            className={`h-[614px] translate-y-20 xl:translate-y-0 pb-20 flex items-center justify-center flex-col relative w-[80%] xl:w-[798px] ${styles.bg} `}
           >
-            <div
-              className={`w-[70%]  h-[400px] xl:h-[614px] translate-y-[200px] !z-[9999999] xl:translate-y-0 pb-20 flex items-center justify-center flex-col relative xl:w-[798px] ${styles.bg} `}
-            >
-              <p className=' text-[10px] mt-10 xl:mt-0 xl:text-[20px] w-[40%] xl:w-[50%] font-normal paytone xl:leading-[23.83px] tracking-[-0.02em] text-center'>
-                Hiya, pals! Welcome to my pineapple under the sea!
-              </p>
-              <p className=' text-[10px] xl:text-[20px] xl:my-[13px] text-[#FFFFFF] w-[55%] paytone font-normal xl:leading-[25.23px] tracking-[-0.03em] text-center'>
-                I’m Spongebob, here to give you a cheerful overview of our
-                fantastic memecoin project.
-              </p>
+            <p className=' text-[10px] mt-10 xl:mt-0 xl:text-[20px] w-[40%] xl:w-[50%] font-normal paytone xl:leading-[23.83px] tracking-[-0.02em] text-center'>
+              Hiya, pals! Welcome to my pineapple under the sea!
+            </p>
+            <p className=' text-[10px] xl:text-[20px] xl:my-[13px] text-[#FFFFFF] w-[55%] paytone font-normal xl:leading-[25.23px] tracking-[-0.03em] text-center'>
+              I’m Spongebob, here to give you a cheerful overview of our
+              fantastic memecoin project.
+            </p>
 
-              <Button
-                className='montserrat transition-all duration-500 hover:!text-[#070707] hover:!bg-white !text-[8px] md:!text-[10px]  xl:!text-[18px] !px-5 !pt-1 !pb-2 xl:!px-[29px] xl:!pt-[14px] xl:!pb-[19px] !font-[600] !bg-[#070707] !text-white'
-                borderColor='#F3D583'
-              >
-                MORE INFO
-              </Button>
-            </div>
+            <Button
+              className='montserrat transition-all duration-500 hover:!text-[#070707] hover:!bg-white !text-[8px] md:!text-[10px]  xl:!text-[18px] !px-5 !pt-1 !pb-2 xl:!px-[29px] xl:!pt-[14px] xl:!pb-[19px] !font-[600] !bg-[#070707] !text-white'
+              borderColor='#F3D583'
+            >
+              MORE INFO
+            </Button>
           </div>
         </div>
       </div>
       <div
-        className={`w-full h-[68px] absolute -bottom-10 ${styles.blur} `}
+        className={`w-[150%] translate-x-[-20%] h-[68px] absolute -bottom-10 ${styles.blur} `}
       ></div>
     </section>
   )
